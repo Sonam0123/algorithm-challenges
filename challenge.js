f6edc7d13eaf0968308c086e4f4f482a7012b449
sonam <tseringsonam34@hotmail.com>
2022-10-17 17:18:14 -0400

    intersection

--- challenge.js
+++ challenge.js
@@ -11,4 +11,23 @@ singleNumber = function(nums) {
         }
     }
 };
-console.log(singleNumber([4,1,2,1,2]));
+// console.log(singleNumber([4,1,2,1,2]));
+
+//finding intersection of two arrays
+function intersect(nums1, nums2){
+    let map = {}
+    let arr = []
+    for(let i = 0; i < nums1.length; i++){
+        map[nums1[i]] = true
+    }
+    for(let j = 0; j < nums2.length; j++){
+        if(arr.length === 2){
+            break
+        }
+        if(map[nums2[j]]){
+            arr.push(nums2[j])
+        }
+    }
+    return arr
+}
+console.log(intersect([1,2,2,1], [2,2]));
