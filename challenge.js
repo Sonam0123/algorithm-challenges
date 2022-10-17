7a4674d16392de123a74ae8c011141ecf2a6ded4
sonam <tseringsonam34@hotmail.com>
2022-10-17 17:33:40 -0400

    sumZero

--- challenge.js
+++ challenge.js
@@ -30,4 +30,23 @@ function intersect(nums1, nums2){
     }
     return arr
 }
-console.log(intersect([1,2,2,1], [2,2]));
+// console.log(intersect([1,2,2,1], [2,2]));
+
+
+//finding the sum in an array that equates to 0
+function sumZero(nums){
+    let left = 0
+    let right = nums.length -1
+    while(left < right){
+        let sum = nums[left] + nums[right]
+        if(sum  === 0){
+            return [nums[left], nums[right]]
+        }
+        else if(sum < 0){
+            left++
+        }else{
+            right--
+        }
+    }
+}
+console.log(sumZero([-4, -2, -1, 0, 1, 2 ,3]));
