81ff6502edeb9c39d236698e0532117cb63e1f9c
sonam <tseringsonam34@hotmail.com>
2022-10-17 22:22:24 -0400

    moving zeroes

--- challenge.js
+++ challenge.js
@@ -86,5 +86,21 @@ var twoSum = function(nums, target) {
         }
     }
 };
-console.log(twoSum([3,2,4], 6));
+// console.log(twoSum([3,2,4], 6));
+
+//move zeroes to the end of the array
+var moveZeroes = function(nums) {
+    let j = 0
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] != 0){
+            nums[j] = nums[i]
+            j++
+        }
+    }
+    for(let i = j; i < nums.length; i++){
+        nums[i] = 0
+    }
+    return nums
+};
+console.log(moveZeroes([0,1,0,3,12]));
 
